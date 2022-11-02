73577e54f042aa7e5339444ae0895f1f5896aee6
Andres Velasco <velasco.andrs@gmail.com>
2022-11-02 16:05:59 -0500

    modify auth type

--- src/auth/types/auth-user.ts
+++ src/auth/types/auth-user.ts
@@ -7,6 +7,6 @@ export interface IAuthUser {
   emailVerified: boolean;
 }
 
-export interface IAuthUserRefreshToken extends IAuthUser{
+export interface IAuthUserRefreshToken extends IAuthUser {
   refreshToken: string;
 }
