b8376a1ceab4088ad6a0692003982f385c1a42fc
Andrés Velasco <velasco.andrs@gmail.com>
2022-11-04 23:36:11 -0500

    refactor email-verified validation into a guard

---
+++ src/auth/guards/email-verified.guard.spec.ts
@@ -0,0 +1,50 @@
+import { ExecutionContext } from '@nestjs/common';
+import { createMock } from '@golevelup/ts-jest';
+import { UnauthorizedException } from '@nestjs/common/exceptions';
+import { EmailVerifiedGuard } from './email-verified.guard';
+
+describe('EmailVerifiedGuard', () => {
+  let emailVerifiedGuard: EmailVerifiedGuard;
+
+  beforeEach(() => {
+    emailVerifiedGuard = new EmailVerifiedGuard();
+  });
+
+  it('should be defined', () => {
+    expect(emailVerifiedGuard).toBeDefined();
+  });
+
+  describe('Can activate', () => {
+    describe('When user email is verified', () => {
+      it('should return true', () => {
+        const mockContext = createMock<ExecutionContext>();
+        mockContext.switchToHttp().getRequest.mockReturnValue({
+          // method attached to `req` instance by Passport lib
+          user: {
+            emailVerified: true,
+          },
+        });
+
+        const canActivate = emailVerifiedGuard.canActivate(mockContext);
+
+        expect(canActivate).toBe(true);
+      });
+    });
+
+    describe('When user email is not verified', () => {
+      it('should throw an error', () => {
+        const mockContext = createMock<ExecutionContext>();
+        mockContext.switchToHttp().getRequest.mockReturnValue({
+          // method attached to `req` instance by Passport lib
+          user: {
+            emailVerified: false,
+          },
+        });
+
+        expect(() => emailVerifiedGuard.canActivate(mockContext)).toThrow(
+          new UnauthorizedException('Email is not verified yet'),
+        );
+      });
+    });
+  });
+});
---
+++ src/auth/guards/email-verified.guard.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { IAuthRequest } from '../types/auth-request';
+
+@Injectable()
+export class EmailVerifiedGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request: IAuthRequest = context.switchToHttp().getRequest();
+
+    if (!request.user?.emailVerified) {
+      throw new UnauthorizedException('Email is not verified yet');
+    }
+
+    return true;
+  }
+}
